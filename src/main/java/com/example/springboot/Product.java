751d7a4287e25ca1935ca357935de0bb00c8e96d
dungntth2009038 <73456416+dungntth2009038@users.noreply.github.com>
2022-06-17 08:59:02 +0700

    spring-boot

---
+++ src/main/java/com/example/springboot/Product.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
+public class Product {
+    private int id;
+    private String name;
+    private String slug;
+    private String thumbnail;
+    private String description;
+    private int status;
+}
---
+++ src/main/java/com/example/springboot/api/HelloController.java
@@ -0,0 +1,4 @@
+package com.example.springboot.api;
+
+public class HelloController {
+}
